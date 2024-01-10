6d2cb759ee0fe1c6bf48976fbafba9777a2aced5
Ammar-Bin-Amir <ammar.amiry7@gmail.com>
2024-01-10 13:46:13 +0500

    Add ram

---
+++ Design/src/ram.sv
@@ -0,0 +1,30 @@
+module ram (
+    input wire clk,
+    input wire [11:0] address,
+    input wire [31:0] data_in,
+    input wire store,
+    input wire load,
+    output logic [31:0] data_out
+);
+    
+    reg [31:0] data [0:4095];
+
+    always @(posedge clk) begin
+        if (store) begin
+            data[address] <= data_in;
+        end
+        else begin
+            data <= data;
+        end
+    end
+
+    always @(posedge clk) begin
+        if (load) begin
+            data_out <= data[address];
+        end
+        else begin
+            data <= data;
+        end
+    end
+
+endmodule
---
+++ Design/tb/tb_ram.sv
@@ -0,0 +1,34 @@
+module tb_ram;
+    
+    timeunit 1ns;
+    timeprecision 1ps;
+
+    logic clk;
+    logic [11:0] address;
+    logic [31:0] data_in;
+    logic store;
+    logic load;
+    wire [31:0] data_out;
+
+    ram dut (
+        .clk,
+        .address,
+        .data_in,
+        .store,
+        .load,
+        .data_out
+    );
+
+    initial clk = 0;
+    always #10 clk = ~clk;
+    
+    initial begin
+        #20 address = 0; data_in = 0; store = 0; load = 0;
+        #25 address = 123; data_in = 32'h1234_cdef;
+        #50 store = 1;
+        #50 load = 1;
+        #10 address = 12'hfff; data_in = 32'hffff_ffff; store = 1; load = 1;
+        #100 $finish;
+    end
+
+endmodule
